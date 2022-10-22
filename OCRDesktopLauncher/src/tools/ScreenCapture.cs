e9f31d77b4208b3cd0091c6048f4a0f3ae799c5b
Lautenschlager <tai.romani@hotmail.com>
2022-10-22 19:30:46 -0300

    fix indentation

--- OCRDesktopLauncher/src/tools/ScreenCapture.cs
+++ OCRDesktopLauncher/src/tools/ScreenCapture.cs
@@ -8,49 +8,49 @@ public static class ScreenCapture
 {
 	private static Image currentImage;
 
-    [DllImport("user32.dll")]
-    private static extern IntPtr GetForegroundWindow();
+	[DllImport("user32.dll")]
+	private static extern IntPtr GetForegroundWindow();
 
-    [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
-    public static extern IntPtr GetDesktopWindow();
+	[DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+	public static extern IntPtr GetDesktopWindow();
 
-    [StructLayout(LayoutKind.Sequential)]
-    private struct Rect
-    {
-        public int Left;
-        public int Top;
-        public int Right;
-        public int Bottom;
-    }   
+	[StructLayout(LayoutKind.Sequential)]
+	private struct Rect
+	{
+		public int Left;
+		public int Top;
+		public int Right;
+		public int Bottom;
+	}
 
-    [DllImport("user32.dll")]
-    private static extern IntPtr GetWindowRect(IntPtr hWnd, ref Rect rect);
+	[DllImport("user32.dll")]
+	private static extern IntPtr GetWindowRect(IntPtr hWnd, ref Rect rect);
 
-    public static void CaptureDesktop()
-    {
-        CaptureWindow(GetDesktopWindow());
-    }
+	public static void CaptureDesktop()
+	{
+		CaptureWindow(GetDesktopWindow());
+	}
 
-    public static void CaptureActiveWindow()
-    {
-        CaptureWindow(GetForegroundWindow());
-    }
+	public static void CaptureActiveWindow()
+	{
+		CaptureWindow(GetForegroundWindow());
+	}
 
-    public static void CaptureWindow(IntPtr handle)
-    {
-        Rect rect = new Rect();
-        GetWindowRect(handle, ref rect);
-        Rectangle bounds =
+	public static void CaptureWindow(IntPtr handle)
+	{
+		Rect rect = new Rect();
+		GetWindowRect(handle, ref rect);
+		Rectangle bounds =
 			new Rectangle(rect.Left, rect.Top, rect.Right - rect.Left, rect.Bottom - rect.Top);
-        Bitmap result = new Bitmap(bounds.Width, bounds.Height);
+		Bitmap result = new Bitmap(bounds.Width, bounds.Height);
 
-        using (Graphics graphics = Graphics.FromImage(result))
-        {
-            graphics.CopyFromScreen(new Point(bounds.Left, bounds.Top), Point.Empty, bounds.Size);
-        }
+		using (Graphics graphics = Graphics.FromImage(result))
+		{
+			graphics.CopyFromScreen(new Point(bounds.Left, bounds.Top), Point.Empty, bounds.Size);
+		}
 
 		currentImage = result;
-    }
+	}
 
 	public static bool Save(string fileName, ImageFormat imageFormat)
 	{
